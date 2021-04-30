7b7e87edded1a2538754798c8fe2b86d20215f5d
Carolina <ydalk3@gmail.com>
2021-04-29 21:17:21 -0500

    pipes personalizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { promise } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   nombre: string = "Capitan América";
-  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  nombre2: string = "caRoLina pORRas gOnZaLeZ";
+  arreglo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   personajes: string[] = ['Ironman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
   PI: number = Math.PI;
   porcentaje: number = 0.234;
   salario: number = 1234.5;
+  fecha: Date = new Date();
+  idioma: string = 'fr';
+  videoUrl: string = "https://www.youtube.com/embed/RAh5xtdIVkA";
+  activar: boolean = true;
 
+
+  heroe = {
+    nombre: 'Logan',
+    calve: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'primera',
+      casa: 20
+    }
+
+
+  }
+
+  valorPromesa = new Promise<string>((resolve) => {
+
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  });
 
 }
---
+++ src/app/pipes/contrasena.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'contrasena'
+})
+export class ContrasenaPipe implements PipeTransform {
+
+  transform(value: string, mostrar: boolean = true): string {
+        
+    return (mostrar) ? '*'.repeat(value.length) : value;
+  }
+
+}
